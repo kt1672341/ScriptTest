496bcc13042a17403ce2877ccfc98de498d0074c
kt1672341 <takeda6726@gmail.com>
2018-07-04 22:34:10 +0900

    ScriptTest
    課題の提出をいたします。

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+        int[] ary = new int[] { 5, 10, 15, 20, 25 };
+
+       
+
+        for (int i = 4; 0 <= ary.Length; i--) {
+
+
+     
+            Debug.Log(ary[i]);
+
+
+
+    }
+
+            }
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
